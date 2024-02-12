54f972cb645fa0ee6501f5919d701fe11c1a4052
Geovanny Silva Ribeiro <geovannysilvaribeiro@MacBook-Air-de-Geovanny.local>
2024-02-12 14:12:08 -0300

    feat: implemented routes subpackage

--- cmd/api/router/router.go
+++ cmd/api/router/router.go
@@ -6,12 +6,8 @@ func Initialize() {
 	//Initializing Router with gin default config
 	r := gin.Default()
 
-	//Defining route
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	//Initialize Routes
+	initializeRoutes(r)
 
 	//Running API server (8080 default port)
 	r.Run(":8080")
