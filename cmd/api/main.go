c7597cb564440a7b8f38ac6fd9495658f04b4e46
Geovanny Silva Ribeiro <geovannysilvaribeiro@MacBook-Air-de-Geovanny.local>
2024-02-12 13:38:13 -0300

    feat: basic API

---
+++ cmd/api/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	//Initializing Router with gin default config
+	r := gin.Default()
+
+	//Defining route
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	//Running API server (8080 default port)
+	r.Run(":8080")
+
+}
